dce5e7272f3d31bfed649f8e9b3d01f77802b207
polinakigim <polinakigim@gmail.com>
2025-03-24 13:16:56 +0300

    Реализация третьего задания: вывод уроков связанных с курсом

--- materials/serializers.py
+++ materials/serializers.py
@@ -9,18 +9,20 @@ class CourseSerializer(ModelSerializer):
         model = Course
         fields = "__all__"
 
-class CourseDetailSerializer(ModelSerializer):
-    number_of_lessons = SerializerMethodField()
-
-    def get_number_of_lessons(self, obj):
-        return obj.lessons.count()
-
-    class Meta:
-        model = Course
-        fields = ("name", "description", "number_of_lessons")
 
 
 class LessonSerializer(ModelSerializer):
     class Meta:
         model = Lesson
         fields = "__all__"
+
+class CourseDetailSerializer(ModelSerializer):
+    number_of_lessons = SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True)
+
+    def get_number_of_lessons(self, obj):
+        return obj.lessons.count()
+
+    class Meta:
+        model = Course
+        fields = ("name", "description", "number_of_lessons", "lessons",)
